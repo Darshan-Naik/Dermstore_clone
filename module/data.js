d91c1d2ae0644bd0320e65eff36f9c0ddafbdcf4
darshan <darshu8314@gmail.com>
2021-03-10 10:49:25 +0530

    landong page 99% with log in log out fuction and data base files added

---
+++ module/data.js
@@ -0,0 +1,190 @@
+export var bio_products = [
+    {
+        src : "https://media.dermstore.com/catalog/500517/300x300/11062.jpg",
+        disp : "LUMIÈRE® Illuminating Eye Cream (0.5 fl. oz.)",
+        price : "$97.00",
+        sale: "Sale: $77.60",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500517/300x300/82326.jpg",
+        disp : " BIO SERUM FIRM Rejuvenating Growth Factor and Peptide Treatment (1 fl. oz.)",
+        price : "$275.00",
+        sale: "Sale: $220.00",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500517/300x300/84137.jpg",
+        disp : " LUMIÈRE® FIRM RICHE Extra Moisturizing Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$118.00",
+        sale: "Sale: $94.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500517/300x300/84138.jpg",
+        disp : " LUMIÈRE® FIRM Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$108.00",
+        sale: "Sale: $86.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500517/300x300/11062.jpg",
+        disp : " NEO Firm Neck & Décolleté Tightening Cream (50 g.)",
+        price : "$135.00",
+        sale: "Sale: $108.00",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500517/300x300/82326.jpg",
+        disp : " BIO SERUM FIRM Rejuvenating Growth Factor and Peptide Treatment (1 fl. oz.)",
+        price : "$275.00",
+        sale: "Sale: $220.00",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500517/300x300/84137.jpg",
+        disp : " LUMIÈRE® FIRM RICHE Extra Moisturizing Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$118.00",
+        sale: "Sale: $94.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500517/300x300/84138.jpg",
+        disp : " LUMIÈRE® FIRM Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$108.00",
+        sale: "Sale: $86.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500517/300x300/84137.jpg",
+        disp : " LUMIÈRE® FIRM RICHE Extra Moisturizing Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$118.00",
+        sale: "Sale: $94.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500517/300x300/84138.jpg",
+        disp : " LUMIÈRE® FIRM Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$108.00",
+        sale: "Sale: $86.40",
+        discount : "20% off with code",
+        code : "GLOWUP"                               
+    }
+]
+
+export var brand_products = [
+    {
+        src : "https://media.dermstore.com/catalog/500300/300x300/20567.jpg",
+        head : "Olaplex",
+        disp : " No. 5 Bond Maintenance™ Conditioner (8.5 fl. oz.)",
+        price : "$28.0",
+        discount : "Earn 2x Dermstore Rewards Points",                       
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500300/300x300/63585.jpg",
+        disp : " BIO SERUM FIRM Rejuvenating Growth Factor and Peptide Treatment (1 fl. oz.)",
+        price : "$275.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500221/300x300/39156.jpg",
+        disp : " LUMIÈRE® FIRM RICHE Extra Moisturizing Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$118.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://media.dermstore.com/catalog/505555/300x300/79300.jpg",
+        disp : " LUMIÈRE® FIRM Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$108.00",
+        discount : "Earn 2x Dermstore Rewards Points",                             
+    },
+    {
+        src : "https://media.dermstore.com/catalog/505555/300x300/79298.jpg",
+        head : "Olaplex",
+        disp : " No. 5 Bond Maintenance™ Conditioner (8.5 fl. oz.)",
+        price : "$28.0",
+        discount : "Earn 2x Dermstore Rewards Points",                       
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500087/300x300/82143.jpg",
+        disp : " BIO SERUM FIRM Rejuvenating Growth Factor and Peptide Treatment (1 fl. oz.)",
+        price : "$275.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://media.dermstore.com/catalog/501690/300x300/74436.jpg",
+        disp : " LUMIÈRE® FIRM RICHE Extra Moisturizing Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$118.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://media.dermstore.com/catalog/505711/300x300/82048.jpg",
+        disp : " LUMIÈRE® FIRM Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$108.00",
+        discount : "Earn 2x Dermstore Rewards Points",                             
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500087/300x300/82133.jpg",
+        disp : " NEO Firm Neck & Décolleté Tightening Cream (50 g.)",
+        price : "$135.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://media.dermstore.com/catalog/501690/300x300/82997.jpg",
+        disp : " BIO SERUM FIRM Rejuvenating Growth Factor and Peptide Treatment (1 fl. oz.)",
+        price : "$275.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://media.dermstore.com/catalog/500221/300x300/47301.jpg",
+        disp : " LUMIÈRE® FIRM RICHE Extra Moisturizing Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$118.00",
+        discount : "Earn 2x Dermstore Rewards Points",                              
+    },
+    {
+        src : "https://media.dermstore.com/catalog/505475/300x300/77221.jpg",
+        disp : " LUMIÈRE® FIRM Illuminating & Tightening Eye Cream (0.5 fl. oz.)",
+        price : "$108.00",
+        discount : "Earn 2x Dermstore Rewards Points",                               
+    }
+]
+
+export var blog_product = [
+    {
+        img : "https://www.dermstore.com/blog/wp-content/uploads/2020/03/skin-care-products-feature-1-300x180.jpg",
+        name : "16 Best Buys from Our Beauty Refresh Event",
+        date : 'Jenn Sinrich 3/3/21'
+    },
+    {
+        img : "https://www.dermstore.com/blog/wp-content/uploads/2021/02/beautyfix-thumbnail-mar-2021-300x180.jpg",
+        name : "March 2021 BeautyFIX Box Reveal",
+        date : '3/2/21'
+    },
+    {
+        img : "https://www.dermstore.com/blog/wp-content/uploads/2021/02/skinceuticals-silymarin-feature-300x180.jpg",
+        name : "Which SkinCeuticals Vitamin C Serum Is Right for You?",
+        date : 'Wendy Rodewald-Sulz 2/22/21'
+    },
+    {
+        img : "https://www.dermstore.com/blog/wp-content/uploads/2015/09/woman-in-the-sunlight-feature-300x180.jpg",
+        name : "6 Things You Didn’t Know About Facial Redness",
+        date : 'Janeca Racho  3/3/21'
+    },
+    {
+        img : "https://www.dermstore.com/blog/wp-content/uploads/2021/01/0820-Blog-Featured-SkinCTripeptide-1-300x180.jpg",
+        name : "8 Best Creams for a Younger-Looking Neck",
+        date : 'Janeca Racho  2/18/21'
+    }
+]
+
+
+
